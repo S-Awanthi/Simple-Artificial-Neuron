01fb49ad8c870d5011ae403b8717c43eccbca3d1
Awanthi <59737260+S-Awanthi@users.noreply.github.com>
2020-06-21 00:49:57 +0530

    Neuron.java

---
+++ Neuron.java
@@ -0,0 +1,11 @@
+
+public class Neuron{
+    
+    int in1, in2, in3, in4;     //4 Inputs      
+    double w1, w2, w3, w4;      //4 Connection weights    
+    
+    double weightedSum;         //Total weighted sum of 4 inputs    
+    double threshold = 0.4;            
+   
+}   
+
